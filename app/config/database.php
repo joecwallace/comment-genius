4b9baf8040526a21adb5c0095bf57f8e8dabb222
Joe C. Wallace, III <joe.c.wallace@gmail.com>
2013-05-03 23:00:11 -0500

    Re-adding migrations to boxfile and adding database creds

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'host'      => 'tunnel.pagodabox.com:3306',
+			'database'  => 'kassandra',
+			'username'  => 'refugio',
+			'password'  => 'dEktPsYS',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
