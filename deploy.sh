991fc5098e79785e78de96c4840d3de0bbc5908b
Joe C. Wallace, III <joe.c.wallace@gmail.com>
2013-05-17 21:18:34 -0500

    Don't forget deps

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
 git checkout -b production
-php artisan cg:build
-php artisan cg:less
+
 echo '' > public/.gitignore
 echo '' > public/css/.gitignore
 echo '' > public/js/.gitignore
+
+cd public && bower install && cd ..
+
+php artisan cg:build
+php artisan cg:less
+
 git add .
 git commit -m "Deploy"
 git push pagoda production:master --force
+
 git checkout master
 git branch -D production
