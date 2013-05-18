4e3e67a74499ea18f7342d1db04e3f10d34fcce9
Joe C. Wallace, III <joe.c.wallace@gmail.com>
2013-05-17 23:09:09 -0500

    Messing with the home page

--- app/views/_partials/home/docs.blade.php
+++ app/views/_partials/home/docs.blade.php
@@ -9,29 +9,26 @@
 
 	<p>Comment Genius comes with a few ways to customize your installation. Um... here they come. Feel free to mix and match.</p>
 
-	<h4>The <code>data-selector</code> attribute</h4>
-	<h6>This is how you decide what gets comment bubbles!</h6>
+	<h4>The data-selector attribute <small>is how you decide what gets comment bubbles!</small></h4>
 	<p>By default, Comment Genius selects all of the <code>&lt;p&gt;</code> elements on your page, but maybe you want something different. You can do that easily with the <code>data-selector</code> attribute. It takes any valid jQuery selector. Like this:</p>
 	<pre>&lt;script <b>data-selector=&quot;#wrapper .content p.commentable&quot;</b>
 	type=&quot;text/javascript&quot; src=&quot;{{ $rootUrl . '/js/comment-genius.js' }}&quot;&gt;
 	&lt;/script&gt;</pre>
 
-	<h4>The <code>data-style</code> attribute</h4>
-	<h6>This is how you style it yourself.</h6>
-	<p>Comment Genius will inject styles into your document's <code>&lt;head /&gt;</code> to make itself look good. To stop that - maybe because you want to style it yourself - use the <code>data-style</code> attribute. It takes <code>"false"</code>; anything else will evaluate to <code>"true"</code>. So, to turn off the default styles, do this:</p>
+	<h4>The data-style attribute <small>is how you style it yourself.</small></h4>
+	<p>Comment Genius will inject styles into your document's <code>&lt;head&gt;</code> to make itself look good. To stop that - maybe because you want to style it yourself - use the <code>data-style</code> attribute. It takes <code>"false"</code>; anything else will evaluate to <code>"true"</code>. So, to turn off the default styles, do this:</p>
 	<pre>&lt;script <b>data-style=&quot;false&quot;</b>
 	type=&quot;text/javascript&quot; src=&quot;{{ $rootUrl . '/js/comment-genius.js' }}&quot;&gt;
 	&lt;/script&gt;</pre>
 
-	<h4>The <code>data-site-id</code> attribute</h4>
-	<h6>This is how you manage comments on your site(s).</h6>
+	<h4>The data-site-id attribute <small>is how you manage comments on your site(s).</small></h4>
+	<p><b>Please note:</b> the admin portion of Comment Genius isn't finished yet. <small>s/isn't finished/has not been started/</small></p>
 	<p>You can manage comments by adding the <code>data-site-id</code> attribute to Comment Genius. To get your Site ID, you have to <a href="#signup-modal" data-toggle="modal">sign up</a>. After that, do something like this:</p>
 	<pre>&lt;script <b>data-site-id=&quot;MY-COMMENT-GENIUS-SITE-ID&quot;</b>
 	type=&quot;text/javascript&quot; src=&quot;{{ $rootUrl . '/js/comment-genius.js' }}&quot;&gt;
 	&lt;/script&gt;</pre>
 
-	<h4>The <code>data-trigger</code> attribute</h4>
-	<h6>This is how you delay execution.</h6>
+	<h4>The data-trigger attribute <small>is how you delay execution.</small></h4>
 	<p>If you don't want to crank up Comment Genius as soon as the page loads, you can stall, or if you want to start Comment Genius before your page is done loading (why?), that could be a thing, too. [By default, Comment Genius will start when the DOM is ready.]</p>
 	<p>First, tell the script not to initialize automatically:</p>
 	<pre>&lt;script <b>data-trigger=&quot;manual&quot;</b>
