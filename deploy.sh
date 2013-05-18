696dfbfa917d818432e87119176b4c32c96a1c5e
Joe C. Wallace, III <joe.c.wallace@gmail.com>
2013-05-17 21:16:38 -0500

    FRUSTRATION

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 git checkout -b production
+php artisan cg:build
+php artisan cg:less
 echo '' > public/.gitignore
+echo '' > public/css/.gitignore
+echo '' > public/js/.gitignore
 git add .
 git commit -m "Deploy"
 git push pagoda production:master --force
